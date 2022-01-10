fb7ced5da405e7910823da5a17062a41ae8acc39
Julham Adhi Putera <000ozmano000@gmail.com>
2022-01-10 17:44:52 +0700

    setup build configuration and migrate from groovy to kts

---
+++ app/build.gradle.kts
@@ -0,0 +1,114 @@
+plugins {
+    id("com.android.application")
+    id("kotlin-android")
+    id("kotlin-kapt")
+    id("dagger.hilt.android.plugin")
+    id("kotlin-parcelize")
+}
+
+android {
+    compileSdkVersion(31)
+    buildToolsVersion("30.0.3")
+
+    defaultConfig {
+        applicationId = "com.kumparan.assessment"
+        minSdkVersion(21)
+        targetSdkVersion(31)
+        versionCode = 1
+        versionName = "1.0"
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    }
+
+    buildFeatures.viewBinding = true
+    buildFeatures.dataBinding = true
+
+    buildTypes {
+        getByName("release") {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
+
+    packagingOptions {
+        exclude("META-INF/*.kotlin_module")
+    }
+
+    kotlinOptions {
+        jvmTarget = "1.8"
+    }
+}
+
+dependencies {
+    // Kotlin
+    implementation(Dependencies.kotlin)
+
+    // Coroutines
+    implementation(Coroutines.core)
+    implementation(Coroutines.android)
+
+    // Android
+    implementation(Android.appcompat)
+    implementation(Android.activityKtx)
+    implementation(Android.coreKtx)
+    implementation(Android.constraintLayout)
+    implementation(Android.swipeRefreshLayout)
+
+    // Architecture Components
+    implementation(Lifecycle.viewModel)
+    implementation(Lifecycle.liveData)
+    implementation(Lifecycle.runtimeKtx)
+
+    //ImageView
+    implementation(Image.circleImageView)
+    implementation(Image.coil)
+
+    // Room components
+    implementation(Room.runtime)
+    implementation(Room.ktx)
+    kapt(Room.compiler)
+
+    // Material Design
+    implementation(Dependencies.materialDesign)
+    implementation(Dependencies.materialDialog)
+
+    // Retrofit
+    implementation(Retrofit.retrofit)
+    implementation(Retrofit.moshiRetrofitConverter)
+
+    // Moshi
+    implementation(Moshi.moshi)
+    implementation(Moshi.codeGen)
+    kapt(Moshi.codeGen)
+
+    // Hilt + Dagger
+    implementation(Hilt.hiltAndroid)
+    implementation(Hilt.hiltViewModel)
+    kapt(Hilt.daggerCompiler)
+    kapt(Hilt.hiltCompiler)
+
+    // Paging
+    implementation(Dependencies.paging)
+
+    // Testing
+    testImplementation(Testing.core)
+    testImplementation(Testing.coroutines)
+    testImplementation(Testing.room)
+    testImplementation(Testing.okHttp)
+    testImplementation(Testing.jUnit)
+    testImplementation(Testing.truth)
+
+    // Android Testing
+    androidTestImplementation(Testing.extJUnit)
+    androidTestImplementation(Testing.espresso)
+    androidTestImplementation(Testing.rule)
+    androidTestImplementation(Testing.okHttp)
+}
